44b558359a6ebbb1727ed407066e1dbf2344a80f
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2016-04-27 18:50:02 +0900

    add exception handler

--- a.cpp
+++ a.cpp
@@ -3,6 +3,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <cstdio>
+#ifdef _WIN32
+  #include <windows.h>
+#endif
 #include "footmark.hpp"
 
 int func_2(void);
@@ -13,9 +16,37 @@ int func_1(void) {
   return 0;
 }
 
+void func_5(void) {
+  FOOTMARK();
+  *(char *)NULL = 0;
+}
+
+void func_4(void) {
+  FOOTMARK();
+  func_5();
+}
+
+void func_3(void) {
+  FOOTMARK();
+  func_4();
+}
+
+#ifdef _WIN32
+  LONG WINAPI MyUnhandledExceptionFilter(PEXCEPTION_POINTERS ExceptionInfo) {
+    FootmarkDebugPrint("### Abnormal Status ###\n");
+    FOOTMARK_PRINT_CALL_STACK();
+    fflush(stdout);
+    return EXCEPTION_EXECUTE_HANDLER;
+  }
+#endif
+
 int main(void) {
   FOOTMARK();
   func_1();
+#ifdef _WIN32
+  ::SetUnhandledExceptionFilter(MyUnhandledExceptionFilter);
+  func_3();
+#endif
   return 0;
 }
 
